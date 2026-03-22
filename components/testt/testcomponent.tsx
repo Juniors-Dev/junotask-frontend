45c740969bd60e11846c13403e732ac97df2cb1e
HelChris <111705707+HelChris@users.noreply.github.com>
2026-03-22 21:13:54 +0100

    test: testing add, commit, push

---
+++ components/testt/testcomponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function test() {
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-2xl font-bold text-heading">fkljg</h1>
+      <p className="text-muted">lwergjljsfdjl sdfkjld</p>
+    </div>
+  );
+}
